152f37a739e84d2edca2a6e5adb5c9d1f88fc8a2
Azazel <80421328+szephyr13@users.noreply.github.com>
2024-11-26 16:45:30 +0100

    bullet movement, partial animations, bug fix

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float speed;
+    [SerializeField] private float shootingRate;
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private GameObject gun;
+    private float timer = 0.5f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,15 +19,31 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Player movement
+        PlayerMovement();
+        MovementLimits();
+        Shoot();
+    }
+
+    private void PlayerMovement()
+    {
         float inputH = Input.GetAxisRaw("Horizontal");
         float inputV = Input.GetAxisRaw("Vertical");
-        transform.Translate(new Vector3(inputH, inputV, 0).normalized * speed * Time.deltaTime);
-
-        //Player limits
-        float clampedY = Mathf.Clamp(inputV, -4.57f, 4.57f);
-        float clampedX = Mathf.Clamp(inputH, -8.31f, 8.48f);
+        transform.Translate(new Vector2(inputH, inputV).normalized * speed * Time.deltaTime);
+        
+    }
+    private void MovementLimits()
+    {
+        float clampedX = Mathf.Clamp(transform.position.x, -8.31f, 8.48f);
+        float clampedY = Mathf.Clamp(transform.position.y, -4.57f, 4.57f);
         transform.position = new Vector3(clampedX, clampedY, 0);
-
+    }
+    private void Shoot()
+    {
+        timer += 1 * Time.deltaTime;
+        if (Input.GetKey(KeyCode.Space) && timer > shootingRate)
+        {
+            Instantiate(bulletPrefab, gun.transform.position, Quaternion.identity);
+            timer = 0;
+        }
     }
 }
